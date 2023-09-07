a60221d631293763b287be20a2119b2ba6ffb825
MALWICK <batahumphrey66@gamil.com>
2023-09-07 17:18:54 +0100

    learned about composition api

--- app.vue
+++ app.vue
@@ -1,7 +1,8 @@
 <template>
-  <HomeWelcome />
-  <Counter />
+  <Home__Welcome />
+  <Component :is="Counter" />
 </template>
 
 <script setup>
+const Counter = resolveComponent("Counter")
 </script>
