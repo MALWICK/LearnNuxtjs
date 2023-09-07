b14e6d43230f0b53d954b23d89c83b2dbb8b2121
MALWICK <batahumphrey66@gamil.com>
2023-09-07 15:58:55 +0100

    solved some errors

--- app.vue
+++ app.vue
@@ -1,34 +1,13 @@
 <template>
-  <div>
-    <div>Hello World</div>
-    <div>
-      <button @click="increment">Increment</button>
-      <p>{{ count }}</p>
-      <button @click="decrement">Decrement</button>
-    </div>
-  </div>
+  
 </template>
 
 <script>
-import { ref } from 'vue';
+
 
 export default {
   setup() {
-    const count = ref(0);
-
-    function increment() {
-      count.value++;
-    }
-
-    function decrement() {
-      count.value--;
-    }
-
-    return {
-      count,
-      increment,
-      decrement
-    };
+    
   },
 };
 </script>
---
+++ components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+  <template>
+    <div>
+      <div>Hello World</div>
+      <div>
+        <button @click="increment">Increment</button>
+        <p>{{ count }}</p>
+        <button @click="decrement">Decrement</button>
+      </div>
+    </div>
+  </template>
+</template>
+
+<script setup>
+const count = ref(0);
+
+function increment() {
+  count.value++;
+}
+
+function decrement() {
+  count.value--;
+}
+</script>
+</template>
